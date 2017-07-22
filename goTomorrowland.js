d62747ed40890a6a1b6d9ffd59977fa02c62a338
bernestoalberto <bernestoalberto@gmail.com>
2017-07-22 12:17:48 -0500

    Tomorroland commit

---
+++ goTomorrowland.js
@@ -0,0 +1,25 @@
+<<<<<<< Updated upstream
+getPlaces(){
+
+let spots = ['bcn','boom','miami','london']
+
+
+return spots;
+
+
+}
+=======
+
+//Function to return the hottest part people in the world
+
+
+getTomorroland(){
+const places = [
+'boom','bcn','ultra'
+
+]
+return
+
+}
+
+>>>>>>> Stashed changes
